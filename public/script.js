4c688824e73e3322454d4e44ff8014f8ed090e58
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-07-19 15:01:44 -0500

    init fingerprint script

--- public/script.js
+++ public/script.js
@@ -1,3 +1,19 @@
+// Initialize the agent at application startup.
+const fpPromise =
+    import ("https://openfpcdn.io/fingerprintjs/v4").then(
+        (FingerprintJS) => FingerprintJS.load()
+    );
+
+// Get the visitor identifier when you need it.
+fpPromise
+    .then((fp) => fp.get())
+    .then((result) => {
+        // This is the visitor identifier:
+        const visitorId = result.visitorId;
+        console.log(visitorId);
+    })
+    .catch((error) => console.error(error));
+
 document.querySelector(".hamburger").addEventListener("click", function() {
     this.classList.toggle("active");
     document.querySelector(".nav-links").classList.toggle("active");
