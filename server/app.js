d77688cb432e117af909b8e66887942759b499f9
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-07-22 22:17:29 -0500

    opened ssl connction

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
-/* jshint esversion: 6, node: true */
 const express = require("express");
 const path = require("path");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const { Pool } = require("pg");
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -11,9 +10,10 @@ env.config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // Note: Setting this to false makes the connection less secure
-  }
+    rejectUnauthorized: false,
+  },
 });
+
 app.use(express.static(path.resolve(__dirname, "..", "public")));
 app.use(bodyParser.json());
 
@@ -21,25 +21,21 @@ app.get("/", function (req, res) {
   res.sendFile(path.resolve(__dirname, "..", "public", "index.html"));
 });
 
-// Assuming you have dotenv and express set up
-app.get('/clientApiKey', (req, res) => {
-  // Implement authentication and authorization checks here
+app.get("/clientApiKey", (req, res) => {
   res.json({ apiKey: process.env.CLIENT_API_KEY });
 });
 
 app.post("/visitorid", async (req, res) => {
-  const { visitorId } = req.body
-
-
   try {
-    // Insert visitorId into the visitors table
-    const queryText = 'INSERT INTO visitors(id) VALUES($1)';
+    const visitorId = req.body.visitorId;
+
+    const queryText = "INSERT INTO visitors(id) VALUES($1)";
     const response = await pool.query(queryText, [visitorId]);
 
-    res.status(200).send('Visitor ID stored successfully');
+    res.status(200).send("Visitor ID stored successfully");
   } catch (error) {
-    console.error('Error storing visitor ID:', error);
-    res.status(500).send('Error storing visitor ID');
+    console.error("Error storing visitor ID:", error);
+    res.status(500).send("Error storing visitor ID");
   }
 });
 
