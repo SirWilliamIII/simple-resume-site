2b85620faacc86efe9d1548b1cdc6eeb3c3256e1
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-08-13 01:47:38 -0500

    removed fingerprinting

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const { Pool } = require("pg");
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const env = require("dotenv");
 
 env.config();
@@ -11,35 +11,29 @@ env.config();
 const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+    connectionString: process.env.DATABASE_URL,
+    ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 app.use(express.static(path.resolve(__dirname, "..", "public")));
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "..", "public", "index.html"));
+app.get("/", function(req, res) {
+    res.sendFile(path.resolve(__dirname, "..", "public", "index.html"));
 });
 
-app.get("/clientApiKey", (req, res) => {
-  res.json({ apiKey: process.env.CLIENT_API_KEY });
+app.post("/visitorid", async(req, res) => {
+    try {
+        const visitorId = req.body.visitorId;
+        const queryText = "INSERT INTO visitors(id) VALUES($1)";
+        const response = await pool.query(queryText, [visitorId]);
+        res.status(200).send("Visitor ID stored successfully");
+    } catch (error) {
+        console.error("Error storing visitor ID:", error);
+        res.status(500).send("Error storing visitor ID");
+    }
 });
 
-app.post("/visitorid", async (req, res) => {
-  try {
-    const visitorId = req.body.visitorId;
-
-    const queryText = "INSERT INTO visitors(id) VALUES($1)";
-    const response = await pool.query(queryText, [visitorId]);
-
-    res.status(200).send("Visitor ID stored successfully");
-  } catch (error) {
-    console.error("Error storing visitor ID:", error);
-    res.status(500).send("Error storing visitor ID");
-  }
-});
-
-app.listen(PORT, function () {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, function() {
+    console.log(`Server is running on port ${PORT}`);
 });
