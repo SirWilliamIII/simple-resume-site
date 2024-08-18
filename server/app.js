37ce9d4f230c4354e5e202a47883310619c64184
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-08-18 04:44:57 -0500

    reformatted resume

--- server/app.js
+++ server/app.js
@@ -5,8 +5,22 @@ const { Pool } = require("pg");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const env = require("dotenv");
+const { auth } = require('express-openid-connect');
+
 
 env.config();
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: 'http://localhost:3000',
+    clientID: 'UuiaVKTf3Twuh8uiIIb6CFR2EKMQohhp',
+    issuerBaseURL: 'https://willcarpenter.auth0.com',
+    secret: 'LONG_RANDOM_STRING'
+};
+
+app.use(auth(config));
+
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -18,11 +32,21 @@ const pool = new Pool({
 app.use(express.static(path.resolve(__dirname, "..", "public")));
 app.use(bodyParser.json());
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.resolve(__dirname, "..", "public", "index.html"));
 });
 
-app.post("/visitorid", async(req, res) => {
+app.get('/login', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+})
+
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
+
+app.post("/visitorid", async (req, res) => {
     try {
         const visitorId = req.body.visitorId;
         const queryText = "INSERT INTO visitors(id) VALUES($1)";
@@ -34,6 +58,6 @@ app.post("/visitorid", async(req, res) => {
     }
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log(`Server is running on port ${PORT}`);
 });
